66c9105b4929d746dbc79f5530eaf667d5c5a4c3
teja7777 <52334571+teja7777@users.noreply.github.com>
2025-03-17 14:55:59 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}" // Ensure this is correctly set
+  ],
   theme: {
     extend: {},
   },
