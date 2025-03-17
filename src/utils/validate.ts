58709ca55fb1a94e6643e0cee3320b613aab7eaf
teja7777 <52334571+teja7777@users.noreply.github.com>
2025-03-17 16:23:56 +0530

    validate the forms by using global function

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,6 +1,6 @@
-export const checkValidateData = (email: string, password: string) => {
-    const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
-    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+export const checkValidateData = (email: any, password: any) => {
+    const isEmailValid = email ? /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email?.value) : false;
+    const isPasswordValid = password ? /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password?.value) : false;
     if (!isEmailValid) return "Email ID is not valid";
     if (!isPasswordValid) return "Password is not valid";
 
