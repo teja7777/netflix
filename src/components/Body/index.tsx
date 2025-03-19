d6b2465635678cfba011a8cea0a0bb5d6c961dce
teja7777 <52334571+teja7777@users.noreply.github.com>
2025-03-19 14:02:45 +0530

    implement new changes for body component

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,9 +1,15 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, useNavigate } from "react-router-dom";
 import BrowseComponent from "../BrowseComponent";
 import Login from "../Login";
 import { RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../utils/firebase";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../../utils/store/Slices/userSlice";
 
 const BodyComp = () => {
+    const dispatch = useDispatch();
     const appRouter = createBrowserRouter([
         {
             path: "/",
@@ -13,7 +19,22 @@ const BodyComp = () => {
             path: "/browse",
             element: <BrowseComponent />
         },
-    ])
+    ]);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                const { uid, email } = user;
+                dispatch(addUser({ uid: uid, email: email }));
+            } else {
+                // User is signed out
+                dispatch(removeUser())
+            }
+        });
+    }, [])
+
+
     return (
         <div>
             <RouterProvider router={appRouter} />
