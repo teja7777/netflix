1e7d1a2b340e43727a0f8ac1b152312cb300c6ab
teja7777 <52334571+teja7777@users.noreply.github.com>
2025-03-19 14:03:40 +0530

    Change the rules for sign in and Signup validates

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,7 +1,15 @@
-export const checkValidateData = (email: any, password: any) => {
+export const checkSignInData = (email: any, password: any) => {
     const isEmailValid = email ? /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email?.value) : false;
     const isPasswordValid = password ? /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password?.value) : false;
     if (!isEmailValid) return "Email ID is not valid";
     if (!isPasswordValid) return "Password is not valid";
 
+}
+export const checkSignUpData = (email: any, password: any, name?: any) => {
+    const isEmailValid = email ? /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email?.value) : false;
+    const isNameValid = name?.value?.length > 4 ? true : false;
+    const isPasswordValid = password ? /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password?.value) : false;
+    if (!isEmailValid) return "Email ID is not valid";
+    if (!isPasswordValid) return "Password is not valid";
+    if (!isNameValid) return "Name must be greater than 4 letters";
 }
