cf7dd6225a3b54da607dc8a71572e37e894ff6d3
gleecus <teja.ulisetti@gleecus.com>
2025-03-20 13:18:39 +0530

    feat:implement movies slice in store

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,9 @@
 export const netflixLogo = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const APIOptions = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNDhmYWYyZDFlZGU4NTNlZGNiNzhhZmMzMTE5YjZhOSIsIm5iZiI6MTc0MjM5MTUwNy40MTIsInN1YiI6IjY3ZGFjOGQzODNkNWYzMmE1NzZjYjM5YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ofP8rS1rR4xAkXhbS9ZPj4OomnBD0J81-T0YuqfoLyY'
+    }
+};
