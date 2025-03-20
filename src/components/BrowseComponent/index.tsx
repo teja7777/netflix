7252ade9e9124bfebf042c95db4a5fb730aa0e1c
teja7777 <52334571+teja7777@users.noreply.github.com>
2025-03-20 13:11:15 +0530

    use TMDB mocies api to get movies list from an API

--- src/components/BrowseComponent/index.tsx
+++ src/components/BrowseComponent/index.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
+import { APIOptions } from "../../utils/constants";
 import Header from "../Header";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../../utils/store/Slices/movieSlice";
 
 const BrowseComponent = () => {
+    const dispatch = useDispatch();
+    const getNowPlayingMovies = async () => {
+        const data = await fetch("https://api.themoviedb.org/3/movie/now_playing", APIOptions);
+        const json = await data.json();
+        dispatch(addNowPlayingMovies(json.results));
+        console.log(json.results);
+    }
+
+    useEffect(() => {
+        getNowPlayingMovies();
+    }, []);
+
     return (
         <div>
             <Header />
